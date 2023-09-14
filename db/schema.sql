9f7a314f34c5aa3dc221e02b9866988e98c2c142
Yuski <yuskijoness@gmail.com>
2023-09-14 15:11:32 -0400

    forgot to push earlier... Completed index.js, database folder, added info and set up mysql

--- db/schema.sql
+++ db/schema.sql
@@ -1,23 +1,65 @@
 
+-- DROP DATABASE IF EXISTS employee_db_2;
 
-CREATE TABLE departments (
-id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-title VARCHAR(30) NOT NULL
+
+-- CREATE DATABASE employee_db_2;
+
+
+USE employee_db_2;
+
+
+-- CREATE TABLE department (
+--   id INT AUTO_INCREMENT PRIMARY KEY,
+--   name VARCHAR(255) NOT NULL
+-- );
+
+
+-- CREATE TABLE `role` (
+--   id INT AUTO_INCREMENT PRIMARY KEY,
+--   title VARCHAR(255) NOT NULL,
+--   salary DECIMAL(10,2) NOT NULL,
+--   department_id INT,
+--   FOREIGN KEY (department_id) REFERENCES department(id)
+-- );
+
+
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(255) NOT NULL,
+  last_name VARCHAR(255) NOT NULL,
+  role_id INT,
+  manager_id INT,
+  FOREIGN KEY (role_id) REFERENCES role(id),
+  FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
 
-CREATE TABLE roles (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(30),
-    salary DECIMAL,
-    department_id INT
-);
 
-CREATE TABLE employees (
-id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-first_name VARCHAR(30),
-last_name VARCHAR(30),
-role_id INT,
-manager_id INT, 
-FOREIGN KEY (role_id) REFERENCES roles(id),
-FOREIGN KEY (manager_id) REFERENCES employees(id)
-);
+
+
+
+
+
+
+
+
+-- CREATE TABLE departments (
+-- id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+-- title VARCHAR(30) NOT NULL
+-- );
+
+-- CREATE TABLE roles (
+--     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+--     title VARCHAR(30),
+--     salary DECIMAL,
+--     department_id INT
+-- );
+
+-- CREATE TABLE employees (
+-- id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+-- first_name VARCHAR(30),
+-- last_name VARCHAR(30),
+-- role_id INT,
+-- manager_id INT, 
+-- FOREIGN KEY (role_id) REFERENCES roles(id),
+-- FOREIGN KEY (manager_id) REFERENCES employees(id)
+-- );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,13 +1,63 @@
-USE employee_db;
+USE employee_db_2;
+
+-- Preform INSERTS one table at a time
+
+INSERT INTO department (`name`) VALUES
+  ('Software Development'),
+  ('Quality Assurance'),
+  ('Product Management');
 
 
-INSERT INTO departments (title)
-VALUES ('Customer Service'), ('Maintenance'),('Marketing'),('Human Resources'),('Management');
+INSERT INTO `role` (title, salary, department_id) VALUES
+  ('Software Engineer', 6000.00, 1),
+  ('UI/UX Designer', 5000.00, 1),
+  ('QA Engineer', 4500.00, 2),
+  ('Product Manager', 7000.00, 3),
+  ('Technical Lead', 8000.00, 1),
+  ('Database Administrator', 5500.00, 1);
 
 
-INSERT INTO roles (title, salary, department_id)
-VALUES ('secretary', 48000, 1), ('Sales Telecaller', 75000, 3),('Janitor', 30000, 2),('HR Director', 75000, 4),('VP',103000,5);
+Need to wait for insert of role before inserting employee
+Check the role id that you need to insert
 
-INSERT INTO employees (first_name, last_name, role_id)
-VALUES ('Ann', 'Perry', 4), ('Patricia', 'Johnson', 1),('Al', 'Jones', 2),('Felix','Martinez',5),('Calvin','Arnold',3);
+INSERT INTO employee (first_name, last_name, role_id) VALUES
+  ('Mary', 'Doe', 8),
+  ('Jane', 'Hope', 9),
+  ('Michael', 'Johnson', 10),
+  ('Emily', 'Wynn',11),
+  ('Larry', 'Brown', 12),
+  ('Sarah', 'Jones', 11),
+  ('Bob', 'Taylor', 7),
+  ('Olivia', 'Miller', 9),
+  ('Nancy', 'Wilson', 8),
+  ('Sophia', 'Anderson', 11);
+
+
+Need to wait for insert of employee before inserting manager_id
+Check the employee id that you need to insert
+
+UPDATE employee SET manager_id = 21 WHERE id = 22;
+UPDATE employee SET manager_id = 21 WHERE id = 24;
+UPDATE employee SET manager_id = 26 WHERE id = 29;
+UPDATE employee SET manager_id = 29 WHERE id = 23;
+UPDATE employee SET manager_id = 29 WHERE id = 27;
+
+
+
+
+
+
+
+-- USE employee_db;
+
+
+-- INSERT INTO departments (title)
+-- VALUES ('Customer Service'), ('Maintenance'),('Marketing'),('Human Resources'),('Management');
+
+
+-- INSERT INTO roles (title, salary, department_id)
+-- VALUES ('secretary', 48000, 1), ('Sales Telecaller', 75000, 3),('Janitor', 30000, 2),('HR Director', 75000, 4),('VP',103000,5);
+
+-- INSERT INTO employees (first_name, last_name, role_id)
+-- VALUES ('Ann', 'Perry', 4), ('Patricia', 'Johnson', 1),('Al', 'Jones', 2),('Felix','Martinez',5),('Calvin','Arnold',3);
 
